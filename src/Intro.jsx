b11d02ff7262b45cd0510e014b336e6e47e8926a
DevelopWithAbhay <soniabhay489@gmail.com>
2025-09-11 20:27:43 +0530

    final push

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -5,15 +5,15 @@ const Intro = () => {
     
     <div className="container flex items-center justify-center my-4">
     <div className=" bg-[#0d0d0d] w-full sm:w-4/5  min-h-[70vh] flex justify-center items-center px-6 sm:px-12 py-10">
-      <div className="container max-w-7xl flex flex-col-reverse md:flex-row items-center md:items-start gap-10">
-        
-        <div className="flex justify-center md:hidden">
-          <img
-            className="w-48 h-48 sm:w-64 sm:h-64 rounded-full border-4 border-gray-700 object-cover shadow-lg"
-            src="/image/profile.jpeg"
-            alt="Profile"
-          />
-        </div>
+      <div className="container  max-w-7xl flex flex-col-reverse md:flex-row items-center md:items-start gap-10">
+      <div className="flex justify-center items-center md:hidden min-h-[60vh]">
+  <img
+    className="w-48 h-48 sm:w-64 sm:h-64 rounded-full border-4 border-gray-700 shadow-lg"
+    src="/image/profile.jpeg"
+    alt="Profile"
+  />
+</div>
+
 
         <div className="flex  flex-col items-center md:items-start text-center md:text-left md:w-2/3">
           <div className="text-white text-center text-3xl sm:text-4xl md:text-4xl font-bold mb-6">
@@ -41,13 +41,14 @@ const Intro = () => {
         </div>
 
         {/* Right Side Image for Large Screens */}
-        <div className="hidden md:flex justify-center items-center md:w-1/3">
-          <img
-            className="w-64 h-64 lg:w-80 lg:h-80 border-4 border-gray-700 rounded-full object-cover shadow-lg"
-            src="image/profile.jpeg"
-            alt="Profile"
-          />
-        </div>
+       <div className="hidden md:flex justify-center items-center md:w-1/3">
+  <img
+    className="w-[100%] h-64 lg:w-80 lg:h-80 border-4 border-gray-700 rounded-full object-center shadow-lg"
+    src="/image/profile.jpeg"
+    alt="Profile"
+  />
+</div>
+
       </div>
     </div>
     </div>
