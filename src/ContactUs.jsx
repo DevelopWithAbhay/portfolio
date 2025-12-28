474bb486136018dee1a0114c1b81edb9df7ff5a9
DevelopWithAbhay <soniabhay489@gmail.com>
2025-12-28 12:46:55 +0530

    soving route issue '

--- src/ContactUs.jsx
+++ src/ContactUs.jsx
@@ -17,7 +17,7 @@ function ContactUs() {
 
   <div className="flex flex-col sm:flex-row gap-4 justify-center">
     <a
-      href="/contact"
+      href="{<Contact/>}"
       className="px-6 py-3 rounded-full bg-white text-black
                  text-sm font-medium hover:bg-gray-200 transition"
     >
--- src/Foooter.jsx
+++ src/Foooter.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Foooter = () => {
   return (
     <div>
-      <div className="footer container mx-auto w-4/5 h-12 bg-[#1B1B1B] flex items-center justify-around gap-12 text-[#dddcdc] text-lg font-sans-serif border-1 rounded-full relative bottom-2 cursor-pointer"> Made With   {"   "}React+TailWind ❤️</div>
+      <div className="footer container mx-auto w-4/5 h-12 bg-[#1B1B1B] bg-red-500 flex items-center justify-around gap-12 text-[#dddcdc] text-lg font-sans-serif border-1 rounded-full relative bottom-2 cursor-pointer"> Made With   {"   "}React+TailWind ❤️</div>
     </div>
   )
 }
